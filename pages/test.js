264b9f3151e0e76a162f04a2d2cdffcf13d5702b
anecrow <anecrow@outlook.com>
2019-03-17 05:20:12 +0800

    构建APP的核心函数方法类

--- pages/test.js
+++ pages/test.js
@@ -1,1 +1,9 @@
-export default ()=><div>empty test</div>
+import React from "react";
+
+import NianLib from "../components/lib/nian-lib";
+
+export default ()=>{
+    const nianlib = new NianLib(DatArchive);
+    nianlib.user.setProfile({name:'test'});
+    return<p>{nianlib.user.getProfile().name}</p>
+}
