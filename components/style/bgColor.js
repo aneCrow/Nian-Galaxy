29219dc780f96a769cd5550fc175e27a0ca27307
anecrow <anecrow@outlook.com>
2019-03-21 08:39:46 +0800

    重写样式 添加flex布局样式

--- components/style/bgColor.js
+++ components/style/bgColor.js
@@ -1,12 +1,13 @@
+import React from "react";
+
 export default ()=>{
     const color = () => {
         const colorList = [ '#EEAB46', '#EFDCC8', '#EEAFAB', '#6D9F99', '#EC666A' ];
         const random = Math.floor(Math.random()*(colorList.length+1));
         return colorList[ random ];
     };
-    return <style jsx global>{
-            `
-                        body {
+    return <style jsx global>{`
+                        html {
                             background: ${color()}
                         }
                     `
